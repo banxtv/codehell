8d3b0acad5d8e8a11266cd79f615a4b229cb6bb2
banxtv <Dustinator123>
2020-03-01 23:52:08 +0100

    creat app

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders app', () => {
+  const { getByText, getClassName } = render(<App />);
+  const linkElement = getByText("Dustin React FizzBuzz");
+  expect(linkElement).toBeInTheDocument();
+});
